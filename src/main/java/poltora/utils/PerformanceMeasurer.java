464b613034e76b93ceae17b9d4a9e2678c774904
Oleg Poltoratskii <opoltoratskii@gainsystems.com>
2025-06-07 23:20:43 +0400

    Display And Reset

--- src/main/java/poltora/utils/PerformanceMeasurer.java
+++ src/main/java/poltora/utils/PerformanceMeasurer.java
@@ -649,6 +649,7 @@ public class PerformanceMeasurer {
 
         private void reset() {
             sensor.reset();
+            history.sensor.reset(); // isUpdated
         }
 
         private boolean isStarted() {
